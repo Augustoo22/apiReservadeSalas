fe64c21c4a4084696947b896e10918eb95560074
Gabriel <ramosga.sk9@gmail.com>
2024-03-28 21:32:11 -0300

    feat: update a URL'S and render template

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from salaDeAula.views.postView import CriarSalaDeAula
 from salaDeAula.views.getView import ListaSalasDeAula, DetalhesSalaDeAula, ListaHorariosDisponiveis
+from salaDeAula.views.renderView import listar_salas
 
 urlpatterns = [
     path('salas/', ListaSalasDeAula.as_view(), name='lista-salas'),
     path('salas/<int:id>/', DetalhesSalaDeAula.as_view(), name='detalhes-sala'),
     path('salas/<int:id>/horarios/', ListaHorariosDisponiveis.as_view(), name='lista-horarios'),
     path('salas/criar/', CriarSalaDeAula.as_view(), name='criar_sala_de_aula'),
+    path('salas/', listar_salas, name='listar_salas'),
 ]
--- salaDeAula/views/renderView.py
+++ salaDeAula/views/renderView.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from ..models import SalaDeAula
 
-def minha_view(request):
-    context = {'chave': 'valor'}  # Contexto opcional para passar dados para o template
-    return render(request, 'meuapp/template.html', context)
+def listar_salas(request):
+    salas = SalaDeAula.objects.all()
+    return render(request, 'listar_salas.html', {'salas': salas})
