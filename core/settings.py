030882934382cebf4c5461cbe80ef1b6377b1918
Augustoo22 <victoraugustodocarmo32@gmail.com>
2024-03-28 21:21:02 -0300

    feat(database) connection mongodb
    database connection to save classroom data

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,10 +79,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'erickGabrielVictor',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb://10.109.2.63:443/',
+        }
     }
 }
+
+
 
 
 # Password validation
